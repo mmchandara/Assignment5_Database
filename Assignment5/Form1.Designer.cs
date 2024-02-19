de873da176ecb388b7992045541e5a9eddce8976
mmchandara <mitchel.chandara@gmail.com>
2024-02-18 22:38:51 -0600

    Add project files.

---
+++ Assignment5/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Assignment5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.txtEmpId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmpGender = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmpHiringDate = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(780, 221);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(571, 284);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Insert Data";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(571, 328);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update data";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(669, 284);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Data";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(669, 328);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 4;
+            this.btnDisplay.Text = "Display Data";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // txtEmpId
+            // 
+            this.txtEmpId.Location = new System.Drawing.Point(99, 262);
+            this.txtEmpId.Name = "txtEmpId";
+            this.txtEmpId.Size = new System.Drawing.Size(416, 20);
+            this.txtEmpId.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "EmpId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "EmpName";
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(99, 288);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(416, 20);
+            this.txtEmpName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "EmpGender";
+            // 
+            // txtEmpGender
+            // 
+            this.txtEmpGender.Location = new System.Drawing.Point(99, 314);
+            this.txtEmpGender.Name = "txtEmpGender";
+            this.txtEmpGender.Size = new System.Drawing.Size(416, 20);
+            this.txtEmpGender.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 343);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "EmpHiringDate";
+            // 
+            // txtEmpHiringDate
+            // 
+            this.txtEmpHiringDate.Location = new System.Drawing.Point(99, 340);
+            this.txtEmpHiringDate.Name = "txtEmpHiringDate";
+            this.txtEmpHiringDate.Size = new System.Drawing.Size(416, 20);
+            this.txtEmpHiringDate.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmpHiringDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmpGender);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmpId);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.TextBox txtEmpId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmpGender;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmpHiringDate;
+    }
+}
+
---
+++ Assignment5/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment5
+{
+    public partial class Form1 : Form
+    {
+       
+        private SQLiteConnection sqlite_conn;
+        public Form1()
+        {
+            InitializeComponent();
+            SQLiteConnection();
+        }
+        private void SQLiteConnection()
+        {
+            sqlite_conn = new SQLiteConnection("Data Source=Employee.db;Version=3;New=True;Compress=True;");
+            try
+            {
+                sqlite_conn.Open();
+                Database();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Database()
+        {
+            using(SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand())
+        {
+                string createTable = "CREATE TABLE IF NOT EXISTS Employees (EmpId INTEGER PRIMARY KEY, EmpName TEXT(50), EmpGender CHAR(1), EmpHiringDate DATE)";
+                sqlite_cmd.CommandText = createTable;
+                sqlite_cmd.ExecuteNonQuery();
+            }
+        }
+        private void ReadData()
+        {
+            try
+            {
+                string query = "SELECT * FROM Employees";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, sqlite_conn);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO Employees (EmpName, EmpGender, EmpHiringDate) VALUES (@EmpName, @EmpGender, @EmpHiringDate)";
+                using (SQLiteCommand add = new SQLiteCommand(query, sqlite_conn))
+                {
+                    add.Parameters.AddWithValue("@EmpName", txtEmpName.Text);
+                    add.Parameters.AddWithValue("@EmpGender", txtEmpGender.Text);
+                    DateTime hiringDate;
+                    if (DateTime.TryParseExact(txtEmpHiringDate.Text, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out hiringDate))
+                    {
+                        add.Parameters.AddWithValue("@EmpHiringDate", hiringDate);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid date format. Please enter the date in the format yyyy-MM-dd.");
+                        return;
+                    }
+
+                    add.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ReadData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "UPDATE Employees SET EmpName=@EmpName, EmpGender=@EmpGender, EmpHiringDate=@EmpHiringDate WHERE EmpId=@EmpId";
+                using (SQLiteCommand update = new SQLiteCommand(query, sqlite_conn))
+                {
+                    update.Parameters.AddWithValue("@EmpName", txtEmpName.Text);
+                    update.Parameters.AddWithValue("@EmpGender", txtEmpGender.Text);
+                    update.Parameters.AddWithValue("@EmpId", txtEmpId.Text);
+                    DateTime hiringDate;
+                    if (DateTime.TryParseExact(txtEmpHiringDate.Text, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out hiringDate))
+                    {
+                        update.Parameters.AddWithValue("@EmpHiringDate", hiringDate.ToString("yyyy-MM-dd"));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid date format. Please enter the date in the format yyyy-MM-dd.");
+                        return;
+                    }
+                    update.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ReadData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "DELETE FROM Employees WHERE EmpId=@EmpId";
+                using (SQLiteCommand delete = new SQLiteCommand(query, sqlite_conn))
+                {
+                    delete.Parameters.AddWithValue("@EmpId", txtEmpId.Text);
+                    delete.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ReadData();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            ReadData();
+        }
+    }
+}
